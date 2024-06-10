de50af439a07cf955aca3860f4f200cc2e81ae25
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-06-10 20:17:55 -0300

    algoritmos de busca e exercicios da lista2

---
+++ projetos/busca-luiz.pl
@@ -0,0 +1,97 @@
+%------------------------------------------
+% Busca em largura/profundidade
+% Por Luiz Eduardo
+%------------------------------------------
+ap([],X,X).
+ap([A|B],C,[A|D]) :- ap(B,C,D).
+
+in(A,[A|_]) :- !.
+in(A,[_|B]) :- in(A,B).
+
+% Implementacao do algoritmo de busca em largura ou profundidade
+% 1 = largura
+% 2 = profundidade
+estrategia(2).
+
+atingemeta([_-E|_]) :- meta(E).
+
+busca([Caminho|_], Solucao) :- atingemeta(Caminho), !, Solucao = Caminho.
+busca([Caminho|Lista], Solucao) :- 
+   findall(UMAEXT, estende(Caminho,UMAEXT), EXT),
+   estrategia(Tipo),
+   estrategia(Tipo),
+   (Tipo = 1 -> ap(Lista, EXT,  Lista1);
+    Tipo = 2 -> ap(EXT, Lista, Lista1)),
+   busca(Lista1, Solucao).
+
+naorepete(_-E,C) :- not(in(_-E,C)).
+
+estende([OperacaoX-EstadoA|Caminho], [OperacaoY-EstadoB,OperacaoX-EstadoA|Caminho]) :-
+   oper(OperacaoY,EstadoA,EstadoB),
+   naorepete(OperacaoY-EstadoB,Caminho).
+
+margem([F,L,C,R], M) :-
+   (F = M -> write('F'); write(' ')),
+   (L = M -> write('L'); write(' ')),
+   (C = M -> write('C'); write(' ')),
+   (R = M -> write('R'); write(' ')).
+
+desenha(Estado) :-
+     write('    '), margem(Estado, a), write('|    |'), margem(Estado,b).
+
+escreve([_-E]) :- write('Estado Inicial: '), write(E), nl, !.
+escreve([O-E|R]) :- 
+    escreve(R), 
+    write('Executando: '), 
+    traduz(O,T),
+    write(T), write(' obtemos '), /*desenha(E),*/ write(E), nl.
+
+resolva :-
+    inicial(X), 
+    busca([[raiz-X]],S), 
+    write(S), nl,
+    escreve(S),
+    write('que é a solução do problema').
+
+%-----------------------------------
+% Especificacao do problema
+%-----------------------------------
+
+traduz(c1, 'encher o jarro 1  ').
+traduz(c2, 'encher o jarro 2  ').
+traduz(v1, 'esvaziar o jarro 1').
+traduz(v2, 'esvaziar o jarro 2').
+traduz(12, 'despejar 1 em 2   ').
+traduz(21, 'despejar 2 em 1   ').
+inicial([0,0]).
+meta([_,2]).
+oper(c1, [X,Y], [3,Y]) :- X < 3.
+oper(c2, [X,Y], [X,4]) :- Y < 4.
+oper(v1, [X,Y], [0,Y]) :- X > 0.
+oper(v2, [X,Y], [X,0]) :- Y > 0.
+oper(12, [X,Y], [0,Y1]) :- X > 0, Y < 4, Y1 is X + Y, Y1 =< 4.
+oper(12, [X,Y], [X1,4]) :- X > 0, Y < 4, X1 is X + Y - 4, X + Y > 4.
+oper(21, [X,Y], [X1,0]) :- Y > 0, X < 3, X1 is X + Y, X1 =< 3.
+oper(21, [X,Y], [3,Y1]) :- Y > 0, X < 3, Y1 is X + Y - 3, X + Y > 3.
+
+% traduz(fa, 'fazendeiro vai sozinho   ').
+% traduz(fb, 'fazendeiro volta sozinho ').
+% traduz(la, 'fazendeiro leva o lobo   ').
+% traduz(lb, 'fazendeiro traz o lobo   ').
+% traduz(ca, 'fazendeiro leva a cabra  ').
+% traduz(cb, 'fazendeiro traz a cabra  ').
+% traduz(ra, 'fazendeiro leva o repolho').
+% traduz(rb, 'fazendeiro traz o repolho').
+
+% oper(fa, [a,L,C,R], [b,L,C,R]) :- L \= C, C \= R.
+% oper(fb, [b,L,C,R], [a,L,C,R]) :- L \= C, C \= R.
+% oper(la, [a,a,C,R], [b,b,C,R]) :- (C \= R; C = b).
+% oper(lb, [b,b,C,R], [a,a,C,R]) :- (C \= R; C = a).
+% oper(ca, [a,L,a,R], [b,L,b,R]).
+% oper(cb, [b,L,b,R], [a,L,a,R]).
+% oper(ra, [a,L,C,a], [b,L,C,b]) :- (L \= C; C = b).
+% oper(rb, [b,L,C,b], [a,L,C,a]) :- (L \= C; C = a).
+
+% % [F,L,C,R], margem em que está cada elemento
+% inicial([a,a,a,a]).
+% meta([b,b,b,b]).
--- projetos/lista2.pl
+++ projetos/lista2.pl
@@ -27,9 +27,98 @@ d(1).
 inverte([X], [X]).
 inverte([X|Y], Z) :- inverte(Y, W), append(W, [X], Z).
 
-comparaLista([A],[A]).
+comparaLista([A],[A]) :- !.
 comparaLista([A|B], [C|D]) :- A == C, comparaLista(B, D).
 
 palindromo(Lista) :- 
     inverte(Lista, ListaInvertida),
     comparaLista(Lista, ListaInvertida).
+
+% EX7
+invert([], []) :- !.
+invert([Cabeca|Calda], ListaInvertida) :- 
+    invert(Calda, ListaAux), 
+    append(ListaAux, [Cabeca], ListaInvertida).
+
+metIguais(Lista) :-
+    invert(Lista, ListaInvertida),
+    (comparaLista(Lista, ListaInvertida) -> 
+        writeln('Yes') ; writeln('No')
+    ).
+
+
+% EX8
+insereOrd(Elemento, [], [Elemento]) :- !.
+insereOrd(Elemento, [Cabeca|Calda], [Elemento,Cabeca|Calda]) :-
+    Elemento @=< Cabeca, !.
+insereOrd(Elemento, [Cabeca|Calda], [Cabeca|ListaOrd]) :-
+    Elemento @> Cabeca, !, insereOrd(Elemento, Calda, ListaOrd).
+
+ordena([],[]) :- !.
+ordena([Cabeca|Calda], ListaOrd) :-
+    ordena(Calda, ListaAux), insereOrd(Cabeca, ListaAux, ListaOrd).
+
+% EX10
+%juntar as duas listas e ordenadas 
+merge(Lista1, Lista2, Resultado) :- 
+    append(Lista1, Lista2, ListasJuntas),
+    ordena(ListasJuntas, Resultado).
+
+% EX11
+/*
+O que está errado no programa abaixo? Rode-o com trace, para:
+?- max(4,3,M)
+?- max(3,4,M)
+max(X,Y,M):-!, X>Y, M=X.
+max(X,Y,M):-!, X<=Y, M=Y.
+
+Resposta => 
+O primeiro erro está na comparação 'X<=Y', em prolog o operador correto seria 'X=<Y'.
+O segundo erro é acontece por causa do corte '!', ele não permite que o backtracking aconteca.
+*/
+
+% EX12
+/*
+O que acontece com o predicado p, abaixo, quando o b é executado?
+a. b. p:-!,a. p:-b.
+
+Resposta => 
+Quando o predicado `b` é executado, ele tem sucesso, e quando o 
+predicado `p` é executado, ele também tem sucesso devido à 
+primeira cláusula `p :- !, a.`, e a segunda cláusula `p :- b.` 
+nunca é considerada por causa do corte (`!`).
+*/
+
+% EX13
+pred1(Lista, Resultado) :- findall([X], member(X, Lista), Resultado).
+pred2(Lista, Resultado) :- findall(pred(X,X), member(X, Lista), Resultado).
+pred3(Lista, Resultado) :- findall([element,X], member(X, Lista), Resultado).
+
+% EX14
+/*
+Considere o seguinte problema: “Há dois jarros com capacidades de 3 e 4 litros,
+respectivamente. Nenhum dos jarros contém qualquer medida ou escala, de modo que só
+se pode saber o conteúdo exato quando eles estão cheios. Sabendo-se que podemos
+encher ou esvaziar um jarro, bem como transferir água de um jarro para outro, encontre
+uma sequência de passos que deixe o jarro de 4 litros com exatamente 2 litros de água”.
+Considere que o estado inicial pode ser representado pela lista [0,0], indicando que os
+jarros de 3 e 4 litros estão vazios inicialmente e a meta é [_,2]. Complete os predicados
+transforma abaixo para descrever todas as transformações possíveis de estados desse
+problema.
+*/
+inicial([0,0]).
+meta([0,2]).
+
+transforma('encher o jarro 1', [X,Y], [3,Y]) :- X < 3.
+transforma('enchar o jarro 2', [X,Y], [X,4]) :- Y < 4.
+transforma('esvaziar o jarro 1', [X,Y], [0,Y]) :- X > 0.
+transforma('esvaziar o jarro 2', [X,Y], [X,0]) :- Y > 0.
+transforma('transferir do jarro 1 para o 2', [X,Y], [0,B]) :- 
+    X > 0, Y < 4, B is X + Y, B =< 4.
+transforma('transferir do jarro 2 para o 1', [X,Y], [A,0]) :-
+    X < 3, Y > 0, A is X + Y, A =< 3.
+%--- considerando que ainda restara agua no jarro de origem
+transforma('transferir do jarro 1 para o 2', [X,Y], [A,4]) :-
+    X > 0, Y < 4, A is X + Y - 4, (X + Y) > 4.
+transforma('transferir do jarro 2 para o 1', [X,Y], [3,B]) :- 
+    X < 3, Y > 0, B is X + Y - 3, (X + Y) > 3.
