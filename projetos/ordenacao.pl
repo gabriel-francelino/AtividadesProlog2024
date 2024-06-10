d2c47b98b5bfeeaf0ee8a3778492d9a7f713e048
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-06-10 15:51:44 -0300

    operacoes de ordenacao

--- projetos/ordenacao.pl
+++ projetos/ordenacao.pl
@@ -9,3 +9,27 @@ ordenacaoPorInsercao([], []) :- !.
 ordenacaoPorInsercao([CabecaLista|CaldaLista], ListaOrdenada) :- 
     ordenacaoPorInsercao(CaldaLista, ListaOrdenadaAux), 
     insereOrd(CabecaLista, ListaOrdenadaAux, ListaOrdenada).
+
+% Ordenacao por selecao
+remove(_, [], []).
+remove(Valor, [Valor|CaldaLista], CaldaLista) :- !.
+remove(Valor, [CabecaLista|CaldaLista], [CabecaLista|ListaResultante]) :-
+    remove(Valor, CaldaLista, ListaResultante).
+
+min([Valor], Valor).
+min([Cabeca|Calda], Cabeca) :- min(Calda, MinimoCalda), Cabeca =< MinimoCalda.
+min([Cabeca|Calda], Minimo) :- min(Calda, Minimo), Cabeca > Minimo.
+
+removeMin(Minimo, Lista, NovaLista) :- 
+    min(Lista, Minimo), remove(Minimo, Lista, NovaLista), !.
+
+ordenacaoPorSelecao([], []) :- !.
+ordenacaoPorSelecao(Lista, [Minimo|ListaOrdenada]) :- 
+    removeMin(Minimo, Lista, ListaAux), ordenacaoPorSelecao(ListaAux, ListaOrdenada), !.
+
+% Ordenacao por troca
+ordenacaoPorTroca(Lista, ListaOrdenada) :- 
+    append(Ord, [A,B|L], Lista), B < A, !,
+    append(Ord, [B,A|L], ListaAux),
+    ordenacaoPorTroca(ListaAux, ListaOrdenada).
+ordenacaoPorTroca(Lista, Lista).
