3bc3cdfd144965104a563e326f09d3a3b394599e
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-06-13 10:34:58 -0300

    implementando quicksort

--- projetos/ordenacao.pl
+++ projetos/ordenacao.pl
@@ -16,9 +16,15 @@ remove(Valor, [Valor|CaldaLista], CaldaLista) :- !.
 remove(Valor, [CabecaLista|CaldaLista], [CabecaLista|ListaResultante]) :-
     remove(Valor, CaldaLista, ListaResultante).
 
-min([Valor], Valor).
-min([Cabeca|Calda], Cabeca) :- min(Calda, MinimoCalda), Cabeca =< MinimoCalda.
-min([Cabeca|Calda], Minimo) :- min(Calda, Minimo), Cabeca > Minimo.
+% min([Valor], Valor).
+% min([Cabeca|Calda], Cabeca) :- min(Calda, MinimoCalda), Cabeca =< MinimoCalda.
+% min([Cabeca|Calda], Minimo) :- min(Calda, Minimo), Cabeca > Minimo.
+
+% Implementacao mais eficiente do min/2
+min([X], [X]).
+min([X|RestoLista], Menor) :- 
+    min(RestoLista, MenorRestoLista),
+    (X < MenorRestoLista -> Menor = X; Menor = MenorRestoLista), !.
 
 removeMin(Minimo, Lista, NovaLista) :- 
     min(Lista, Minimo), remove(Minimo, Lista, NovaLista), !.
@@ -29,7 +35,21 @@ ordenacaoPorSelecao(Lista, [Minimo|ListaOrdenada]) :-
 
 % Ordenacao por troca
 ordenacaoPorTroca(Lista, ListaOrdenada) :- 
-    append(Ord, [A,B|L], Lista), B < A, !,
-    append(Ord, [B,A|L], ListaAux),
+    append(Antes, [A,B|Depois], Lista), B < A, !,
+    append(Antes, [B,A|Depois], ListaAux),
     ordenacaoPorTroca(ListaAux, ListaOrdenada).
 ordenacaoPorTroca(Lista, Lista).
+
+% Ordenacao por QuickSort
+particao([X|RestoLista], Pivo, [X|Menores], Maiores) :- 
+    X < Pivo, !, particao(RestoLista, Pivo, Menores, Maiores).
+particao([X|RestoLista], Pivo, Menores, [X|Maiores]) :- 
+    X >= Pivo, !, particao(RestoLista, Pivo, Menores, Maiores).
+particao([], _, [], []).
+
+quickSort([Pivo|RestoLista], ListaOrdenada) :-
+    particao(RestoLista, Pivo, Menores, Maiores),
+    quickSort(Menores, MenoresOrd),
+    quickSort(Maiores, MaioresOrd),
+    append(MenoresOrd, [Pivo|MaioresOrd], ListaOrdenada).
+quickSort([],[]).
